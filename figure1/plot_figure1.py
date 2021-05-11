6c9afcbc367497cb54700a55f167a613753a7f50
Julian Bender <asperrimus@posteo.de>
2021-05-11 11:04:12 +0200

    Sup Figure 2A
    Small correction for reading txt files from MassLynx

--- figure1/plot_figure1.py
+++ figure1/plot_figure1.py
@@ -13,20 +13,36 @@ import numpy as np
 
 os.chdir(r'C:\Users\User\Documents\08_publications\20210220_ijms\figure1')
 
-panels = [x for x in os.listdir() if os.path.isdir(x)]
-fig, axs = plt.subplots(4,len(panels), figsize=(8,5), squeeze=False, sharex=True, sharey=True, frameon=False)
 
 cm = plt.get_cmap('cool')
 
-for ia, p in enumerate(panels):
-    spectra = glob('{}/*.txt'.format(p))
-    for ib, s in enumerate(spectra):
-        mz, intens = np.genfromtxt(s, delimiter='\t', unpack=True)
-        intens = 100*intens/max(intens) # relative intensity
-        axs[ib, ia].plot(mz, intens, color=cm(1-1.*ib/len(spectra)),
-                         label='{}-fold'.format(int(s.split('\\')[1].split('_')[0])/100))
+def group_by_sum(x):
+    """
+    Required as MassLync saves multiple data per m/z bin
+    """
+    u, idx = np.unique(x[:,0], return_inverse=True)
+    s = np.bincount(idx, weights = x[:,1])
+    return np.c_[u, s]
 
-        axs[ib, ia].legend()
+#%%
+
+spectra = glob('../data/figure1/*.txt')
+fig, axs = plt.subplots(len(spectra), #nrows
+                        1, #ncols
+                        figsize=(8,5),
+                        squeeze=False,
+                        sharex=True,
+                        sharey=True,
+                        frameon=False)
+
+for ib, s in enumerate(spectra):
+    d = np.genfromtxt(s, delimiter='\t', unpack=False)
+    mz, intens = group_by_sum(d).T
+    intens = 100*intens/max(intens) # relative intensity
+    axs[ib, 0].plot(mz, intens, color=cm(1-1.*ib/len(spectra)),
+                     label='{}-fold'.format(int(s.split('\\')[1].split('_')[0])/100))
+
+    axs[ib, 0].legend()
 
 plt.xlim([2000,10000])
 plt.savefig('figure1.pdf')
--- figure2/plot_figure2a_sup.py
+++ figure2/plot_figure2a_sup.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 22 08:52:13 2021
+Created on Mon May 10 15:42:16 2021
 
 @author: User
 """
-
 import matplotlib.pyplot as plt
 # plt.style.use('ggplot')
 from glob import glob
 import os, sys
 import numpy as np
 
-sys.path.append('..')
-from functions import *
-
 os.chdir(r'C:\Users\User\Documents\08_publications\20210220_ijms\figure2')
 
+
+sys.path.append('..')
+from functions import *
 panels = [x for x in os.listdir() if os.path.isdir(x) and not (x.startswith('.') or x.startswith('__'))]
 fig, axs = plt.subplots(4,len(panels), figsize=(8,5), squeeze=False, frameon=False)
 
@@ -36,6 +35,7 @@ for ia, p in enumerate(panels):
     for ib, s in enumerate(spectra):
         d = np.genfromtxt(s, delimiter='\t', unpack=False)
         mz, intens = group_by_sum(d).T
+        
         intens = 100*intens/max(intens) # relative intensity
         l  = axs[ib, ia].plot(mz, intens, color=cm(1-1.*ib/len(spectra)),
                          label=s.split('\\')[1].split('_')[0])
@@ -44,18 +44,18 @@ for ia, p in enumerate(panels):
     
         # set ylabels
         if ia == 0:
-            axs[ib, ia].set_xlim([4000,10000])
+            axs[ib, ia].set_xlim([1000,12000])
             if ib == 1:
                 axs[ib, ia].set_ylabel('Relative Intensity [%]')
             if ib == 0:
                 axs[ib, ia].title.set_text('ADH')
         else:
             if ia == 1:
-                axs[ib, ia].set_xlim([1000,6000])
+                axs[ib, ia].set_xlim([1000,12000])
                 if ib == 0:
                     axs[ib, ia].title.set_text('B-Lac')
             elif ia == 2:
-                axs[ib, ia].set_xlim([2000,11000])
+                axs[ib, ia].set_xlim([2000,12000])
                 if ib == 0:
                     axs[ib, ia].title.set_text('BSA')
             axs[ib, ia].set_ylabel(None)
@@ -70,36 +70,5 @@ for ia, p in enumerate(panels):
             axs[ib, ia].get_xaxis().set_ticklabels([])
 
 
-plt.savefig('figure2.pdf')
+plt.savefig('figure2a_sup.pdf')
 plt.show()
-
-#%% Figure 2B: ADH IMS R1
-
-subs, av = ims_plot(r'C:\Users\User\Documents\02_experiments\16_protein_detergent_native_ms\analysis\detergent_ims\ADH_R1\*ADH*.csv',
-                     147000,
-                     2000,
-                     30,
-                     "figure2B",
-                     "ADH R1",
-                     figsize=(4,4))
-
-#%% Figure 2Sup1: BSA IMS R1
-
-subs, av= ims_plot([r'C:\Users\User\Documents\02_experiments\16_protein_detergent_native_ms\analysis\detergent_ims\BSA_R1\20210225_TK_BSA_80_80.csv',
-                  r'C:\Users\User\Documents\02_experiments\16_protein_detergent_native_ms\analysis\detergent_ims\BSA_R1\20210225_TK_C8E4_BSA_80_80.csv',
-                  r'C:\Users\User\Documents\02_experiments\16_protein_detergent_native_ms\analysis\detergent_ims\BSA_R1\20210225_TK_LDAO_BSA_80_80.csv',
-                  r'C:\Users\User\Documents\02_experiments\16_protein_detergent_native_ms\analysis\detergent_ims\BSA_R1\20210225_TK_OG_BSA_80_80_1CMC.csv'],
-                 66000,
-                 2000,
-                 10,
-                 "figure2Sup1",
-                 "BSA R1")
-
-#%% Fig 2Sup2: ADH IMS R2
-
-subs, av = ims_plot(r'C:\Users\User\Documents\02_experiments\16_protein_detergent_native_ms\analysis\detergent_ims\ADH_R2\*ADH*.csv',
-                     147000,
-                     2000,
-                     10,
-                     "figure2Sup2",
-                     "ADH R2")
